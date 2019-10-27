4eda313dfcd1283a203bf1812153608335d27781
chitralimbu <limbu_nikkil@hotmail.co.uk>
2019-10-27 19:51:56 +0000

    Docker push script added

---
+++ autoBuildPush.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mvn clean install -DskipTests
+sudo docker rm chitralimbu
+sudo docker image remove chitralimbu
+sudo docker build -f Dockerfile -t chitralimbu .
+sudo docker tag chitralimbu chitralimbu/chitralimbu:latest
+sudo docker push chitralimbu chitralimbu/chitralimbu:latest

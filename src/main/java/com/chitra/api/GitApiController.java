c004feaf50d730312149c98a704c5bf734a21b20
Chitra Limbu <limbu_nikkil@hotmail.co.uk>
2019-12-22 18:37:22 +0000

    .

--- src/main/java/com/chitra/api/GitApiController.java
+++ src/main/java/com/chitra/api/GitApiController.java
@@ -1,20 +1,21 @@
 package com.chitra.api;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.chitra.resource.GitRepositoryResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import com.chitra.domain.GitRepository;
 import com.chitra.repository.GitRepositoryRepository;
 import com.chitra.service.GitRepositoryService;
 import com.google.gson.Gson;
+
+import javax.xml.ws.Response;
 
 @RestController
 @RequestMapping("/api/git")
@@ -33,19 +34,17 @@ public class GitApiController {
 		return allRepositories;
 	}
 	
-	@GetMapping("/all")
-	@ResponseStatus(HttpStatus.OK)
-	public List<GitRepository> allRepository(){
-		return gitRepo.findAll();
+	@GetMapping(produces = "application/hal+json")
+	public ResponseEntity<List<GitRepositoryResource>> allRepository(){
+		return new ResponseEntity<List<GitRepositoryResource>>(gitRepo.findAll().stream().map(GitRepositoryResource::new).collect(Collectors.toList()), HttpStatus.OK);
 	}
 	
-	@GetMapping("/{repository}")
-	@ResponseStatus(HttpStatus.OK)
-	public GitRepository getRepository(@PathVariable("repository") String repository) {
-		return gitRepo.findByName(repository);
+	@GetMapping(value = "/{repository}", produces = "application/hal+json")
+	public ResponseEntity<GitRepositoryResource> getRepository(@PathVariable("repository") String repository) {
+		return new ResponseEntity<GitRepositoryResource>(new GitRepositoryResource(gitRepo.findByName(repository)), HttpStatus.OK);
 	}
 	
-	@GetMapping("/refresh/{repository}")
+	@PutMapping(value = "/refresh/{repository}", produces = "application/hal+json")
 	@ResponseStatus(HttpStatus.OK)
 	public GitRepository updateRepository(@PathVariable("repository") String repository, RestTemplate restTemplate, Gson gson) {
 		return gitRepoService.updateRepository(repository, restTemplate, gson);
--- src/main/java/com/chitra/resource/GitRepositoryResource.java
+++ src/main/java/com/chitra/resource/GitRepositoryResource.java
@@ -3,12 +3,14 @@ package com.chitra.resource;
 import com.chitra.api.GitApiController;
 import com.chitra.controller.CodeController;
 import com.chitra.domain.GitRepository;
+import lombok.Getter;
 import org.springframework.hateoas.ResourceSupport;
 import org.springframework.ui.Model;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+@Getter
 public class GitRepositoryResource extends ResourceSupport {
 
     private final GitRepository gitRepository;
@@ -16,7 +18,8 @@ public class GitRepositoryResource extends ResourceSupport {
     public GitRepositoryResource(final GitRepository gitRepository){
         this.gitRepository = gitRepository;
         final String name = gitRepository.getName();
+        add(linkTo(GitApiController.class).slash(name).withSelfRel());
         add(linkTo(CodeController.class).slash(name).withRel("url"));
-        add(linkTo(GitApiController.class).slash(name).withRel("local-git-api"));
+        add(linkTo(GitApiController.class).withRel("all"));
     }
 }
